3d916b74e7476d03233a16a1e65046fb2d980387
ss7sudesh <sudeshkumardas7@gmail.com>
2020-06-29 21:59:22 +0530

    Updates

---
+++ precache-manifest.4a62f1d45abf19b409f86adc51c582af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1931d9fa93e504c0afcf3b7655573acc",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3754622be659ec32579d",
+    "url": "/tic-tac-toe/static/css/main.2367ff67.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3754622be659ec32579d",
+    "url": "/tic-tac-toe/static/js/main.87ae93b9.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
